5e647d2d5d5843c68d8891e861929ce3db8ec938
priegelsdev <priegelsdev@gmail.com>
2023-01-25 21:48:35 +0100

    add comments to lay out what to do next

--- src/Answer.jsx
+++ src/Answer.jsx
@@ -1,9 +1,11 @@
-export default function Answers() {
+export default function Answers(props) {
 
   return (
     <div className="answer">
-      
-    </div>
+    <h2 className="question-text">{props.question}</h2>
+    {/* add logic to make 4 questions appear at random positions */}
+    <span> { /*{props.correct_answer, props.incorrect.answers[0-2]}*/ } placeholder </span>
+  </div>
   )
 
 }
--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,19 @@ import Intro from './Intro'
 import Question from './Question'
 
 export default function App() {
+  // save data to state to be displayed in either questions/answers view
+  // (figure out exact logic on the way)
+  
   fetch('https://opentdb.com/api.php?amount=5&type=multiple')
   .then(res => res.json())
   .then(data => console.log(data))
+
+  // plan for now: create two view states when clicking button: questions & answer
+  // in those create state for selected item and 
+  // apply colors in css
+
+
+  // put props in components and add consts/functions to add multiple questions/answers
 
   return (
     <main>
